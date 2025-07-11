e8ad692850819dff6c8800cd864cf25d85d7e2ba
ComplianceGGPH <compliance@ggpholdings.com>
2025-07-11 11:04:30 +0800

    forgot to adjust sheet range

--- api/check-ic.js
+++ api/check-ic.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 
         const result = await sheets.spreadsheets.values.get({
             spreadsheetId: process.env.SHEET_ID,
-            range: "'Form responses 1'!A2:O",
+            range: "'Form responses 1'!A2:P",
         });
 
         const rows = result.data.values;
