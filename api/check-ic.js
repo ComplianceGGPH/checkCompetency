cc8271c8c13827e552033bb73635a3eaf99f43d8
ComplianceGGPH <compliance@ggpholdings.com>
2025-07-18 12:15:35 +0800

    mbad i forgot to edit a line of code

--- api/check-ic.js
+++ api/check-ic.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
         });
 
         const rows = result.data.values;
-        const matched = rows.find((row) => row[23] === icNumber);
+        const matched = rows.find((row) => row[3] === icNumber);
 
         if (matched) {
             return res.json({
